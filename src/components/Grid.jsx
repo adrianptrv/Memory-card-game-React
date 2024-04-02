592bc854969447fe8f115c8f72cebcfa4b2bd7cb
adrianptrv <adrian.prtv1@gmail.com>
2024-04-02 17:59:06 +0300

    New changes

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,28 +1,74 @@
 import { useState, useEffect } from "react";
 
 
+fetch("https://api.unsplash.com/search/photos?query=dog&client_id=nl2Ap7tFASL8YLZndqojgkL4o2wwGE5OqRCWR6b853Y")
+.then((response) => response.json())
+.then((data) => {
+    let test2 = data.results
+    console.log(test2[3].urls.small)
+})
+
 function Person() {
-  const [person, setPerson] = useState(["box1", "box2", "box3", "box4", "box5"]);
+  const [blocks, setBlocks] = useState(["box1", "box2", "box3", "box4", "box5"]);
 
+  //Clicked list
+  const [clickedBox, setClickedBox] = useState([]);
 
+  //Score
+  const [score, setScore] = useState(0);
 
+  //Shuffle the pictures function
   const Shuffle = () => {
-    let arr = [...person]
+    let arr = [...blocks]
     arr.sort(() => Math.random() - 0.5)
-    setPerson(arr)
+    setBlocks(arr)
   }
 
+  //New game button function 
+  const NewGame = () => {
+    setBlocks(["box1", "box2", "box3", "box4", "box5"])
+    setClickedBox([]);
+    setScore(0)
+  }
+
+
+ //Main function
   const Comparer = (child) => {
-   console.log(child)
-   console.log(person.includes(child))
+
+    //Checks if the picture was already clicked. And if was, ends the game.
+    if (clickedBox.includes(child)) {
+      alert("GAME OVER! Your score is: " + score);
+
+      //Resets varibles to default
+      NewGame();
+    }
+    else {
+      //Increases the score
+      setScore(score + 1);
+
+      //Checks if the player clicked all of the pictures
+      if (clickedBox.length == 4) {
+        alert("Congrats you Won!", {score})
+      }
+
+      //If he still haven't, adds the clicked one to the "clicked" list, and Shuffle the pictures
+      else {
+        const currentArr = clickedBox;
+        currentArr.push(child);
+        setClickedBox(currentArr); 
+        Shuffle();
+      }
+    }
   }
 
   return (
     <>
-      {person.map((child, i) => <div key={i} value={child} onClick={() => Comparer(child)} style={{ border: "3px solid black" }}>
+      {blocks.map((child, i) => <div key={i} value={child} onClick={() => Comparer(child)} style={{ border: "3px solid black" }}>
         <h1>{child}</h1>
       </div>)}
       <button onClick={Shuffle}>Click</button>
+      <button onClick={NewGame}>New game</button>
+      <p>Score: {score}</p>
 
     </>
   );
