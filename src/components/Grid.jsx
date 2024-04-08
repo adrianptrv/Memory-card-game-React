84ae9472b5cb85007655a8764c356dec9d081c04
adrianptrv <adrian.prtv1@gmail.com>
2024-04-08 14:06:32 +0300

    Finishing changes

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,21 +3,24 @@ import "../styles/Grid.scss"
 
 let firstArr = []
 
-function Grid({wordSearch}) {
+function Grid({ wordSearch, handleHomeScreen }) {
 
-
+ //Calls the API with the desired category name *wordSearch* and when it gets the results, adds them to an array
   useEffect(() => {
     fetch("https://api.unsplash.com/search/photos?query=" + wordSearch + "&client_id=nl2Ap7tFASL8YLZndqojgkL4o2wwGE5OqRCWR6b853Y")
       .then((response) => response.json())
       .then((data) => {
+        //Gets the raw data results
         let resultJson = data.results
         let arr1 = []
+        //Pushesh the first 10 results, image urls into an array
         for (let i = 0; i <= 9; i++) {
           arr1.push(resultJson[i].urls.small)
         }
+        //Sets the blocks array which we are using to show the picture blocks in out grid, with the arr1 array in which we stored the result from the API.
         setBlocks(arr1)
+        //Sets the arr1 result to the firstArr variable, so we can have the default view of the grid, when we are starting a new game.
         firstArr = arr1;
-        console.log(arr1)
       })
 
   }, [])
@@ -31,6 +34,12 @@ function Grid({wordSearch}) {
   //Score
   const [score, setScore] = useState(0);
 
+  //High score
+  const [highScore, setHighScore] = useState(0);
+
+  //Modal trigger
+  const [modalT, setModalT] = useState(0);
+
   //Shuffle the pictures function
   const Shuffle = () => {
     let arr = [...blocks]
@@ -40,11 +49,18 @@ function Grid({wordSearch}) {
 
   //New game button function 
   const NewGame = () => {
+    //Sets the block images to the default values they were.
     setBlocks(firstArr)
+    //Resets the clicked boxes array to empty for the new game.
     setClickedBox([]);
+    //Resets the score.
     setScore(0)
   }
 
+  //Handle modal change 
+  const HandleModalChange = () => {
+    setModalT(!modalT)
+  }
 
   //Main function
   const Comparer = (child) => {
@@ -53,6 +69,10 @@ function Grid({wordSearch}) {
     if (clickedBox.includes(child)) {
       alert("GAME OVER! Your score is: " + score);
 
+      //Checks and updates the high score
+      if (highScore != score && highScore < score) {
+        setHighScore(score)
+      }
       //Resets varibles to default
       NewGame();
     }
@@ -62,7 +82,9 @@ function Grid({wordSearch}) {
 
       //Checks if the player clicked all of the pictures
       if (clickedBox.length == 9) {
-        alert("Congrats you Won!")
+        setHighScore(10)
+        HandleModalChange();
+        NewGame();
       }
 
       //If he still haven't, adds the clicked one to the "clicked" list, and Shuffle the pictures
@@ -77,14 +99,27 @@ function Grid({wordSearch}) {
 
   return (
     <>
-    <div className="gridWrapper">
-      {blocks.map((child, i) => <div className="gridItem" key={i} value={child} onClick={() => Comparer(child)}>
-        <img className="gridImg" src={child}></img>
-      </div>)}
-      </div>
-      <button className="btn" onClick={NewGame}>New game</button>
+   <div>
+      <button className="btn new" onClick={NewGame}>New game</button>
+      <div className="scores">
       <p>Score: {score}</p>
-
+      <p>High score: {highScore}</p>
+      </div>
+      <div className="gridWrapper">
+        {/* Maps the whole array and creates block for every img */}
+        {blocks.map((child, i) => <div className="gridItem" key={i} value={child} onClick={() => Comparer(child)}>
+          <img className="gridImg" src={child}></img>
+        </div>)}
+      </div>
+      </div> 
+      {/* Modal switch */}
+      {modalT ? <div className="modalScreen">
+        <div className="btnWrapper">
+      <h1>YOU WON</h1>
+      <button onClick={HandleModalChange}>New game</button>
+      <button onClick={handleHomeScreen}>New search</button>
+      </div>
+    </div> : <></>} 
     </>
   );
 }
--- src/components/SearchScreen.jsx
+++ src/components/SearchScreen.jsx
@@ -1,18 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import Grid from "./Grid.jsx"
 import "../styles/SearchScreen.scss"
 
 
 function SearchScreen() {
+    //Used for changing pages between the search page and the grid(game) page
     const [pageNum, setPageNum] = useState(1)
+    //Holds the value of the input
     const [inputWord, setInputWord] = useState()
 
+    //Funtion for chaging pages
     const handlePageChange = () => {
         setPageNum(!pageNum)
     }
 
     return (
         <>
+        {/* Tenary for the page changing */}
             {pageNum ?
                 <div className="searchWrapper">
                     <h1>Please enter a category with which you would like to play.</h1>
@@ -21,7 +25,7 @@ function SearchScreen() {
                     <br></br>
                     <button className="btn" onClick={handlePageChange}>Start game</button>
                 </div>
-                : <Grid wordSearch={inputWord} />}
+                : <Grid wordSearch={inputWord} handleHomeScreen={handlePageChange} />}
         </>
     );
 }
